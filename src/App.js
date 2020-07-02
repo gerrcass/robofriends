e31fc21879654d57248224c08885954c7bf23d7e
Gerardo Castillo <gerrcass@gmail.com>
2020-07-02 09:39:15 -0400

    adding state to the component so that CardList get filtered

--- src/App.js
+++ src/App.js
@@ -6,12 +6,31 @@ import { robots } from "./robots.js";
 import SearchBox from "./SearchBox";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
   render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
     return (
       <div className="tc">
         <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={robots} />;
+        <SearchBox
+          searchfield={this.state.searchfield}
+          searchChange={this.onSearchChange}
+        />
+        <CardList robots={filteredRobots} />;
       </div>
     );
   }
