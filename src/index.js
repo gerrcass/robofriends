ba2e7bd991696b8756b8dbd132fba31d54c1785a
Gerardo Castillo <gerrcass@gmail.com>
2020-07-03 10:53:41 -0400

    adding redux logger

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
 import { searchRobots } from "./reducers.js";
 import "./index.css";
 import App from "./containers/App";
 import "tachyons";
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
